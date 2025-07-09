7c3aa603195cd8b3c82fe555a7abd284ff9cb582
Nivednivu <142215596+Nivednivu@users.noreply.github.com>
2025-07-09 17:22:34 +0530

    Update index.js cors

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express()
 const cors = require("cors")
 const router = require('./router.js')
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://mmsfront-end.vercel.app', // allow frontend origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json())
 app.use(router)
 
